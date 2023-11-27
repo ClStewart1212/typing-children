cce783f0f074cb816e5211543c3dc6ede492c7a3
Clayton Stewart <claytons1212@gmail.com>
2023-11-27 11:34:20 -0700

    type

--- src/components/application.tsx
+++ src/components/application.tsx
@@ -8,13 +8,17 @@
  * React.ReactChild[];
  */
 
-type BoxProps = { children: any /* 👈 Get rid of this! */ };
+import React, { PropsWithChildren } from 'react';
 
-const Box = ({ children }: BoxProps) => {
+type BoxProps = PropsWithChildren<{
+  style?: React.CSSProperties;
+}>;
+
+const Box = ({ children, style }: BoxProps) => {
   return (
     <section
       className="m-4"
-      style={{ padding: '1em', border: '5px solid purple' }}
+      style={{ padding: '1em', border: '5px solid purple', ...style }}
     >
       {children}
     </section>
